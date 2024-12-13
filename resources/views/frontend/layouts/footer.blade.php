4706561657a3b02cc8ce50dabe3237a712c02c1a
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-13 17:16:19 +0700

    asdf234234

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,212 +1,77 @@
- <!-- footer start -->
- <footer class="footer">
-     <div class="footer_top">
-         <div class="container">
-             <div class="row">
+<!-- Footer Start -->
+<footer class="footer">
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <!-- Column 1: Logo and Description -->
                 <div class="col-xl-3 col-md-6 col-lg-3">
                     <div class="footer_widget">
                         <figure class="image-logo">
-                            <img src="{{ asset(@$footerInfo->logo) }}" alt="" class="logo-footer">
+                            <img src="{{ asset(@$footerInfo->logo) }}" alt="Logo" class="logo-footer">
                         </figure>
-
                         <p>{{ @$footerInfo->description }}</p>
-
-
                         <div class="social__media mt-4">
                             <ul class="list-inline">
                                 @foreach ($socialLinks as $link)
                                     <li class="list-inline-item">
-                                        <a href="{{ $link->url }}"
-                                            class="btn btn-social rounded text-white facebook">
+                                        <a href="{{ $link->url }}" class="btn btn-social rounded text-white facebook">
                                             <i class="{{ $link->icon }}"></i>
                                         </a>
                                     </li>
                                 @endforeach
-
                             </ul>
                         </div>
                     </div>
                 </div>
-                 <div class="col-xl-3 col-md-6 col-lg-3">
-                     <div class="footer_widget">
-                         <h3 class="footer_title">
-                             {{ @$footerGridOneTitle->value }}
-                         </h3>
-                         <ul>
-                             @foreach ($footerGridOne as $gridOne)
-                                 <li>
-                                     <a href="{{ $gridOne->url }}">{{ $gridOne->name }}</a>
-                                 </li>
-                             @endforeach
-                         </ul>
 
-                     </div>
-                 </div>
-                 <div class="col-xl-2 col-md-6 col-lg-2">
-                     <div class="footer_widget">
-                         <h3 class="footer_title">
-                             {{ @$footerGridTwoTitle->value }}
-                         </h3>
-                         <ul>
-                             @foreach ($footerGridTwo as $gridTwo)
-                                 <li>
-                                     <a href="{{ $gridTwo->url }}">{{ $gridTwo->name }}</a>
-                                 </li>
-                             @endforeach
-                         </ul>
-                     </div>
-                 </div>
-                 <div class="col-xl-3 col-md-6 col-lg-3">
-                     <div class="footer_widget">
-                         <h3 class="footer_title">
-                             {{ @$footerGridThreeTitle->value }}
-                         </h3>
-                         <ul>
-                             @foreach ($footerGridThree as $gridThree)
-                                 <li>
-                                     <a href="{{ $gridThree->url }}">{{ $gridThree->name }}</a>
-                                 </li>
-                             @endforeach
-                         </ul>
-                     </div>
-                 </div>
-                 <div class="col-xl-4 col-md-6 col-lg-4">
-                     <div class="footer_widget">
-                         <h3 class="footer_title">
-                             Get More Information !
-                         </h3>
+                <!-- Column 2: Grid One Links -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">{{ @$footerGridOneTitle->value }}</h3>
+                        <ul>
+                            @foreach ($footerGridOne as $gridOne)
+                                <li><a href="{{ $gridOne->url }}">{{ $gridOne->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
 
-                         <form action="#" class="newsletter_form">
-                             <input type="email" placeholder="Enter your email" name="email" required>
-                             <button type="submit" class="newsletter-button">Get Info!</button>
-                         </form>
+                <!-- Column 3: Grid Two Links -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">{{ @$footerGridTwoTitle->value }}</h3>
+                        <ul>
+                            @foreach ($footerGridTwo as $gridTwo)
+                                <li><a href="{{ $gridTwo->url }}">{{ $gridTwo->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
 
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-     <div class="copy-right_text">
-         <div class="container">
-             <div class="footer_border"></div>
-             <div class="row">
-                 <div class="col-xl-12">
-                     <p class="copy_right text-center">
-                         {{ @$footerInfo->copyright }}</p>
-                     </p>
-                 </div>
-             </div>
-         </div>
-     </div>
- </footer>
+                <!-- Column 4: Newsletter -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">Get More Information!</h3>
+                        <form action="#" class="newsletter_form">
+                            <input type="email" placeholder="Enter your email" name="email" required>
+                            <button type="submit" class="newsletter-button">Get Info!</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-
- <footer>
-     <div class="wrapper__footer bg__footer-dark pb-0">
-         <div class="container">
-             <div class="row">
-                 <div class="col-md-4">
-                     <div class="widget__footer">
-                         <figure class="image-logo">
-                             <img src="{{ asset(@$footerInfo->logo) }}" alt="" class="logo-footer">
-                         </figure>
-
-                         <p>{{ @$footerInfo->description }}</p>
-
-
-                         <div class="social__media mt-4">
-                             <ul class="list-inline">
-                                 @foreach ($socialLinks as $link)
-                                     <li class="list-inline-item">
-                                         <a href="{{ $link->url }}"
-                                             class="btn btn-social rounded text-white facebook">
-                                             <i class="{{ $link->icon }}"></i>
-                                         </a>
-                                     </li>
-                                 @endforeach
-
-                             </ul>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-md-3">
-                     <div class="widget__footer">
-                         <div class="dropdown-footer">
-                             <h4 class="footer-title">
-                                 {{ @$footerGridOneTitle->value }}
-                                 <span class="fa fa-angle-down"></span>
-                             </h4>
-
-                         </div>
-
-                         <ul class="list-unstyled option-content is-hidden">
-
-                             @foreach ($footerGridOne as $gridOne)
-                                 <li>
-                                     <a href="{{ $gridOne->url }}">{{ $gridOne->name }}</a>
-                                 </li>
-                             @endforeach
-
-                         </ul>
-                     </div>
-                 </div>
-                 <div class="col-md-3">
-                     <div class="widget__footer">
-                         <div class="dropdown-footer">
-                             <h4 class="footer-title">
-                                 {{ @$footerGridTwoTitle->value }}
-                                 <span class="fa fa-angle-down"></span>
-                             </h4>
-
-                         </div>
-                         <ul class="list-unstyled option-content is-hidden">
-
-                             @foreach ($footerGridTwo as $gridTwo)
-                                 <li>
-                                     <a href="{{ $gridTwo->url }}">{{ $gridTwo->name }}</a>
-                                 </li>
-                             @endforeach
-
-                         </ul>
-                     </div>
-                 </div>
-                 <div class="col-md-2">
-                     <div class="widget__footer">
-                         <div class="dropdown-footer">
-                             <h4 class="footer-title">
-                                 {{ @$footerGridThreeTitle->value }}
-                                 <span class="fa fa-angle-down"></span>
-                             </h4>
-
-                         </div>
-
-                         <ul class="list-unstyled option-content is-hidden">
-                             @foreach ($footerGridThree as $gridThree)
-                                 <li>
-                                     <a href="{{ $gridThree->url }}">{{ $gridThree->name }}</a>
-                                 </li>
-                             @endforeach
-                         </ul>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-
-     <!-- Footer bottom -->
-     <div class="wrapper__footer-bottom bg__footer-dark">
-         <div class="container ">
-             <div class="row">
-                 <div class="col-md-12">
-                     <div class="border-top-1 bg__footer-bottom-section">
-                         <p class="text-white text-center">
-                             {{ @$footerInfo->copyright }}</p>
-
-                     </div>
-
-                 </div>
-             </div>
-         </div>
-     </div>
- </footer>
+    <!-- Copyright Section -->
+    <div class="copy-right_text">
+        <div class="container">
+            <div class="footer_border"></div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <p class="copy_right text-center">{{ @$footerInfo->copyright }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
