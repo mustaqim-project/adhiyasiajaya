8d105d770c8e1d91929efbd32b2f50d4f24560c6
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-14 06:37:14 +0700

    asdfasdf234234

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -149,36 +149,50 @@
     </div>
 
 
-    <!-- our_customers_area -->
-    <div class="our_customers_area">
+    <div class="service_area">
         <div class="container">
-            <div class="section_title mb-50 text-center">
-                <h3>Our Customers</h3>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title mb-50 text-center">
+                        <h3>
+                            Our Customer
+                        </h3>
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <div class="col-xl-12">
-                    <div class="customer_slider owl-carousel">
-                        <div class="single_customer">
-                            <img src="https://asset.kompas.com/crops/dD9uTk_-ef4kegb1_TYy3Wpiz40=/24x0:824x533/750x500/data/photo/2022/02/20/6211f3d476f3e.jpg" alt="Customer 1">
+                    <div class="service_active owl-carousel">
+                        <div class="single_category">
+                            <div class="thumb">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVA18_ofxfkZJmXY0TMHQLkkIv3GXcv8rmyg&s" alt="APP Group">
+                            </div>
+                            <div class="service_info">
+                                <h3><a href="#">APP Group</a></h3>
+                            </div>
                         </div>
-                        <div class="single_customer">
-                            <img src="https://asset.kompas.com/crops/dD9uTk_-ef4kegb1_TYy3Wpiz40=/24x0:824x533/750x500/data/photo/2022/02/20/6211f3d476f3e.jpg" alt="Customer 2">
+                        <div class="single_category">
+                            <div class="thumb">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9JOtXW_2BTkrNacxEKzqcC_vLQgNDDk-7iw&s" alt="APRIL Group">
+                            </div>
+                            <div class="service_info">
+                                <h3><a href="#">APRIL Group</a></h3>
+                            </div>
                         </div>
-                        <div class="single_customer">
-                            <img src="https://asset.kompas.com/crops/dD9uTk_-ef4kegb1_TYy3Wpiz40=/24x0:824x533/750x500/data/photo/2022/02/20/6211f3d476f3e.jpg" alt="Customer 3">
-                        </div>
-                        <div class="single_customer">
-                            <img src="https://asset.kompas.com/crops/dD9uTk_-ef4kegb1_TYy3Wpiz40=/24x0:824x533/750x500/data/photo/2022/02/20/6211f3d476f3e.jpg" alt="Customer 4">
-                        </div>
-                        <div class="single_customer">
-                            <img src="https://asset.kompas.com/crops/dD9uTk_-ef4kegb1_TYy3Wpiz40=/24x0:824x533/750x500/data/photo/2022/02/20/6211f3d476f3e.jpg" alt="Customer 5">
+                        <div class="single_category">
+                            <div class="thumb">
+                                <img src="https://media.licdn.com/dms/image/v2/D4E0BAQF4cdabJF0DHQ/company-logo_200_200/company-logo_200_200/0/1686619480470/dayasa_aria_prima_logo?e=2147483647&v=beta&t=T8WjLPQdIc1ILMuZM6SomN0UF80O4RBdPSYH0wMrEGU" alt="DAYASA ARIA PRIMA">
+                            </div>
+                            <div class="service_info">
+                                <h3><a href="#">DAYASA ARIA PRIMA</a></h3>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--/ our_customers_area -->
+
 
 
 
@@ -244,7 +258,7 @@
 
 
 
-    <!-- counter_area  -->
+    {{-- <!-- counter_area  -->
     <div class="counter_area">
         <div class="container">
             <div class="offcan_bg">
@@ -278,7 +292,7 @@
 
         </div>
     </div>
-    <!-- /counter_area  -->
+    <!-- /counter_area  --> --}}
 
 
     <script>
