66c5b745b6c65edf425e1b2025f7e3cad935174c
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-13 16:42:54 +0700

    SDFG345345DFG

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -50,13 +50,12 @@
                         <h3 class="footer_title">
                             Subscribe
                         </h3>
-                        <form action="" class="newsletter-form">
+                        <form action="#" class="newsletter-form">
                             <input type="text" placeholder="Enter your mail">
                             <button type="submit">Subscribe</button>
                         </form>
 
-                        <p class="newsletter_text">Esteem spirit temper too say adieus who direct esteem esteems
-                            luckily.</p>
+
                     </div>
                 </div>
             </div>
