e1510b99019aecc25249b4a299f4c20fc5f3a18f
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-13 19:53:07 +0700

    wert23423sdfg

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,89 +1,91 @@
-
-
-
-
-<!-- header-start -->
+<!-- Header Start -->
 <header>
-    <div class="header-area ">
+    <div class="header-area">
+        <!-- Header Top Area -->
         <div class="header-top_area d-none d-lg-block">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-xl-4 col-lg-4">
                         <div class="logo">
-                            <a href="index.html">
-                                <img src="img/logo.png" alt="">
+                            <a href="{{ url('/') }}">
+                                <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid">
                             </a>
                         </div>
                     </div>
-                    <div class="col-xl-8 col-md-8">
+                    <div class="col-xl-8 col-lg-8">
                         <div class="header_right d-flex align-items-center">
                             <div class="short_contact_list">
                                 <ul>
-                                    <li><a href="#"> <i class="fa fa-envelope"></i> info@docmed.com</a></li>
-                                    <li><a href="#"> <i class="fa fa-phone"></i> 1601-609 6780</a></li>
+                                    <li>
+                                        <a href="mailto:{{ @$contact->email }}">
+                                            <i class="fa fa-envelope"></i> {{ @$contact->email }}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="tel:{{ @$contact->phone }}">
+                                            <i class="fa fa-phone"></i> {{ @$contact->phone }}
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
-
                             <div class="book_btn d-none d-lg-block">
-                                <a class="boxed-btn3-line" href="#">Get Free Quote</a>
+                                <a class="boxed-btn3-line" href="{{ route('contact') }}">Claim Your Quote!</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- Sticky Header -->
         <div id="sticky-header" class="main-header-area">
             <div class="container">
                 <div class="header_bottom_border">
                     <div class="row align-items-center">
+
+                        <!-- Mobile Logo -->
                         <div class="col-12 d-block d-lg-none">
                             <div class="logo">
                                 <a href="index.html">
-                                    <img src="img/logo.png" alt="">
+                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid">
                                 </a>
                             </div>
                         </div>
+
+                        <!-- Main Navigation -->
                         <div class="col-xl-9 col-lg-9">
-                            <div class="main-menu  d-none d-lg-block">
+                            <div class="main-menu d-none d-lg-block">
                                 <nav>
                                     <ul id="navigation">
-                                        <li><a  href="index.html">home</a></li>
-                                        <li><a  href="service.html">Services</a></li>
-                                        <li><a href="About.html">about</a></li>
-                                        <li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                            <ul class="submenu">
-                                                <li><a href="service_details.html">service details</a></li>
-                                                <li><a href="elements.html">elements</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">blog <i class="ti-angle-down"></i></a>
-                                            <ul class="submenu">
-                                                <li><a href="blog.html">blog</a></li>
-                                                <li><a href="single-blog.html">single-blog</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="{{ url('/') }}">{{ __('frontend.Home') }}</a></li>
+                                        <li><a href="{{ route('about') }}">{{ __('frontend.About Us') }}</a></li>
+                                        <li><a href="{{ route('product') }}">{{ __('frontend.Our Product') }}</a></li>
+                                        <li><a href="{{ route('contact') }}">{{ __('frontend.Contact') }}</a></li>
                                     </ul>
                                 </nav>
                             </div>
                         </div>
+
+                        <!-- Search Button -->
                         <div class="col-xl-3 col-lg-3 d-none d-lg-block">
                             <div class="Appointment justify-content-end">
                                 <div class="search_btn">
-                                    <a data-toggle="modal" data-target="#exampleModalCenter" href="#">
+                                    <a data-toggle="modal" data-target="#searchModal" href="#">
                                         <i class="ti-search"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
+
+                        <!-- Mobile Menu -->
                         <div class="col-12">
                             <div class="mobile_menu d-block d-lg-none"></div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
 </header>
-<!-- header-end -->
+<!-- Header End -->
+
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -13,22 +13,24 @@
         @endif
     </title>
 
-    <meta name="description" content="
+    <meta name="description"
+        content="
         @hasSection('meta_description')
-            @yield('meta_description')
-        @else
-            {{ $settings['site_seo_description'] }}
-        @endif" />
+@yield('meta_description')
+@else
+{{ $settings['site_seo_description'] }}
+@endif" />
     <meta name="keywords" content="{{ $settings['site_seo_keywords'] }}" />
 
     <meta name="og:title" content="@yield('meta_og_title')" />
     <meta name="og:description" content="@yield('meta_og_description')" />
-    <meta name="og:image" content="
+    <meta name="og:image"
+        content="
         @hasSection('meta_og_image')
-            @yield('meta_og_image')
-        @else
-            {{ asset($settings['site_logo']) }}
-        @endif" />
+@yield('meta_og_image')
+@else
+{{ asset($settings['site_logo']) }}
+@endif" />
 
     <meta name="twitter:title" content="@yield('meta_tw_title')" />
     <meta name="twitter:description" content="@yield('meta_tw_description')" />
@@ -67,132 +69,24 @@
         $footerGridOne = \App\Models\FooterGridOne::where(['status' => 1, 'language' => getLangauge()])->get();
         $footerGridTwo = \App\Models\FooterGridTwo::where(['status' => 1, 'language' => getLangauge()])->get();
         $footerGridThree = \App\Models\FooterGridThree::where(['status' => 1, 'language' => getLangauge()])->get();
-        $footerGridOneTitle = \App\Models\FooterTitle::where(['key' => 'grid_one_title', 'language' => getLangauge()])->first();
-        $footerGridTwoTitle = \App\Models\FooterTitle::where(['key' => 'grid_two_title', 'language' => getLangauge()])->first();
-        $footerGridThreeTitle = \App\Models\FooterTitle::where(['key' => 'grid_three_title', 'language' => getLangauge()])->first();
+        $footerGridOneTitle = \App\Models\FooterTitle::where([
+            'key' => 'grid_one_title',
+            'language' => getLangauge(),
+        ])->first();
+        $footerGridTwoTitle = \App\Models\FooterTitle::where([
+            'key' => 'grid_two_title',
+            'language' => getLangauge(),
+        ])->first();
+        $footerGridThreeTitle = \App\Models\FooterTitle::where([
+            'key' => 'grid_three_title',
+            'language' => getLangauge(),
+        ])->first();
         $contact = \App\Models\Contact::where('language', getLangauge())->first();
     @endphp
 
     <!-- Header -->
-    {{-- @include('frontend.layouts.header') --}}
-<!-- Header Start -->
-<header>
-    <div class="header-area">
-        <!-- Header Top Area -->
-        <div class="header-top_area d-none d-lg-block">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-4 col-lg-4">
-                        <div class="logo">
-                            <a href="{{ url('/') }}">
-                                <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-xl-8 col-lg-8">
-                        <div class="header_right d-flex align-items-center">
-                            <div class="short_contact_list">
-                                <ul>
-                                    <li>
-                                        <a href="mailto:{{ @$contact->email }}">
-                                            <i class="fa fa-envelope"></i> {{ @$contact->email }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="tel:{{ @$contact->phone }}">
-                                            <i class="fa fa-phone"></i> {{ @$contact->phone }}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="book_btn d-none d-lg-block">
-                                <a class="boxed-btn3-line" href="{{ route('contact') }}">Claim Your Quote!</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    @include('frontend.layouts.header')
 
-        <!-- Sticky Header -->
-        <div id="sticky-header" class="main-header-area">
-            <div class="container">
-                <div class="header_bottom_border">
-                    <div class="row align-items-center">
-
-                        <!-- Mobile Logo -->
-                        <div class="col-12 d-block d-lg-none">
-                            <div class="logo">
-                                <a href="index.html">
-                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid">
-                                </a>
-                            </div>
-                        </div>
-
-                        <!-- Main Navigation -->
-                        <div class="col-xl-9 col-lg-9">
-                            <div class="main-menu d-none d-lg-block">
-                                <nav>
-                                    <ul id="navigation">
-                                        <li><a href="{{ url('/') }}">{{ __('frontend.Home') }}</a></li>
-                                        <li><a href="{{ route('about') }}">{{ __('frontend.About Us') }}</a></li>
-                                        <li><a href="{{ route('product') }}">{{ __('frontend.Our Product') }}</a></li>
-                                        <li><a href="{{ route('contact') }}">{{ __('frontend.Contact') }}</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-
-                        <!-- Search Button -->
-                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
-                            <div class="Appointment justify-content-end">
-                                <div class="search_btn">
-                                    <a data-toggle="modal" data-target="#searchModal" href="#">
-                                        <i class="ti-search"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Search Modal -->
-                        <div class="modal fade" id="searchModal" tabindex="-1" role="dialog"
-                            aria-labelledby="searchModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <form action="{{ route('product') }}" method="GET">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="searchModalLabel">Search Product</h5>
-                                            <button type="button" class="close" data-dismiss="modal"
-                                                aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <input type="text" name="search" class="form-control"
-                                                placeholder="Search for products">
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-primary">Search</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <!-- Mobile Menu -->
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
-<!-- Header End -->
     <!-- Content -->
     @yield('content')
 
@@ -200,7 +94,30 @@
     @include('frontend.layouts.footer')
 
     <!-- Modal -->
-    <div class="modal fade custom_search_pop" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <!-- Search Modal -->
+    <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <form action="{{ route('product') }}" method="GET">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="searchModalLabel">Search Product</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="text" name="search" class="form-control" placeholder="Search for products">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="modal fade custom_search_pop" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="serch_form">
@@ -209,7 +126,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- JS here -->
     <script src="{{ asset('depan/js/vendor/modernizr-3.5.0.min.js') }}"></script>
