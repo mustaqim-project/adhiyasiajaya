621265b67634c2b87ee05c7152c24cb6d66ea80e
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-13 17:24:26 +0700

    sa453245fdg

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -4,6 +4,84 @@
         <div class="container">
             <div class="row">
                 <!-- Column 1: Logo and Description -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <figure class="image-logo">
+                            <img src="{{ asset(@$footerInfo->logo) }}" alt="Logo" class="logo-footer">
+                        </figure>
+                        <p>{{ @$footerInfo->description }}</p>
+                        <div class="social__media mt-4">
+                            <ul class="list-inline">
+                                @foreach ($socialLinks as $link)
+                                    <li class="list-inline-item">
+                                        <a href="{{ $link->url }}" class="btn btn-social rounded text-white facebook">
+                                            <i class="{{ $link->icon }}"></i>
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Column 2: Grid One Links -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">{{ @$footerGridOneTitle->value }}</h3>
+                        <ul>
+                            @foreach ($footerGridOne as $gridOne)
+                                <li><a href="{{ $gridOne->url }}">{{ $gridOne->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Column 3: Grid Two Links -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">{{ @$footerGridTwoTitle->value }}</h3>
+                        <ul>
+                            @foreach ($footerGridTwo as $gridTwo)
+                                <li><a href="{{ $gridTwo->url }}">{{ $gridTwo->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Column 4: Newsletter -->
+                <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="footer_widget">
+                        <h3 class="footer_title">Get More Information!</h3>
+                        <form action="#" class="newsletter_form">
+                            <input type="email" placeholder="Enter your email" name="email" required>
+                            <button type="submit" class="newsletter-button">Get Info!</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Copyright Section -->
+    <div class="copy-right_text">
+        <div class="container">
+            <div class="footer_border"></div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <p class="copy_right text-center">{{ @$footerInfo->copyright }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
+<!-- Footer Start -->
+{{-- <footer class="footer">
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <!-- Column 1: Logo and Description -->
                 <div class="col-xl-2 col-md-6 col-lg-2">
                     <div class="footer_widget">
                         <figure class="image-logo">
@@ -48,7 +126,7 @@
                     </div>
                 </div>
 
-                {{-- <!-- Column 4: Grid Three Links -->
+                <!-- Column 4: Grid Three Links -->
                 <div class="col-xl-2 col-md-6 col-lg-2">
                     <div class="footer_widget">
                         <h3 class="footer_title">{{ @$footerGridThreeTitle->value }}</h3>
@@ -58,7 +136,7 @@
                             @endforeach
                         </ul>
                     </div>
-                </div> --}}
+                </div>
 
                 <!-- Column 5: Newsletter -->
                 <div class="col-xl-4 col-md-6 col-lg-4">
@@ -85,5 +163,5 @@
             </div>
         </div>
     </div>
-</footer>
+</footer> --}}
 <!-- Footer End -->
