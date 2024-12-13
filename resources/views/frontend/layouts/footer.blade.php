b2a0332004bbad08bc7f6a47554e45e24f9401c6
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-13 10:32:10 +0700

    asdfasdfjl2kj34324

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -50,12 +50,21 @@
                         <h3 class="footer_title">
                             Subscribe
                         </h3>
+
+                        <form action="" class="newsletter-form">
+                            <div class="input-group ">
+                                <input type="text" class="form-control" name="email" placeholder="Your email address">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary newsletter-button" type="submit">{{ __('frontend.sign up') }}</button>
+                                </div>
+                            </div>
+                        </form>
+
                         <form action="#" class="newsletter_form">
-                            <input type="text" placeholder="Enter your mail">
+                                <input type="text" class="form-control" name="email" placeholder="Your email address">
                             <button type="submit">Subscribe</button>
                         </form>
-                        <p class="newsletter_text">Esteem spirit temper too say adieus who direct esteem esteems
-                            luckily.</p>
+
                     </div>
                 </div>
             </div>
@@ -67,17 +76,121 @@
             <div class="row">
                 <div class="col-xl-12">
                     <p class="copy_right text-center">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script> All rights reserved | This template is made with <i
-                            class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                            target="_blank">Colorlib</a>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        {{ @$footerInfo->copyright }}</p>
                     </p>
                 </div>
             </div>
         </div>
+    </div>
+</footer>
+
+
+
+<footer>
+    <div class="wrapper__footer bg__footer-dark pb-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="widget__footer">
+                        <figure class="image-logo">
+                            <img src="{{ asset(@$footerInfo->logo) }}" alt="" class="logo-footer">
+                        </figure>
+
+                        <p>{{ @$footerInfo->description }}</p>
+
+
+                        <div class="social__media mt-4">
+                            <ul class="list-inline">
+                                @foreach ($socialLinks as $link)
+                                <li class="list-inline-item">
+                                    <a href="{{ $link->url }}" class="btn btn-social rounded text-white facebook">
+                                        <i class="{{ $link->icon }}"></i>
+                                    </a>
+                                </li>
+                                @endforeach
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="widget__footer">
+                        <div class="dropdown-footer">
+                            <h4 class="footer-title">
+                                {{ @$footerGridOneTitle->value }}
+                                <span class="fa fa-angle-down"></span>
+                            </h4>
+
+                        </div>
+
+                        <ul class="list-unstyled option-content is-hidden">
+
+                            @foreach ($footerGridOne as $gridOne)
+                            <li>
+                                <a href="{{ $gridOne->url }}">{{ $gridOne->name }}</a>
+                            </li>
+                            @endforeach
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="widget__footer">
+                        <div class="dropdown-footer">
+                            <h4 class="footer-title">
+                                {{ @$footerGridTwoTitle->value }}
+                                <span class="fa fa-angle-down"></span>
+                            </h4>
+
+                        </div>
+                        <ul class="list-unstyled option-content is-hidden">
+
+                            @foreach ($footerGridTwo as $gridTwo)
+                            <li>
+                                <a href="{{ $gridTwo->url }}">{{ $gridTwo->name }}</a>
+                            </li>
+                            @endforeach
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="widget__footer">
+                        <div class="dropdown-footer">
+                            <h4 class="footer-title">
+                                {{ @$footerGridThreeTitle->value }}
+                                <span class="fa fa-angle-down"></span>
+                            </h4>
+
+                        </div>
+
+                        <ul class="list-unstyled option-content is-hidden">
+                            @foreach ($footerGridThree as $gridThree)
+                            <li>
+                                <a href="{{ $gridThree->url }}">{{ $gridThree->name }}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer bottom -->
+    <div class="wrapper__footer-bottom bg__footer-dark">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="border-top-1 bg__footer-bottom-section">
+                        <p class="text-white text-center">
+                            {{ @$footerInfo->copyright }}</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </footer>
