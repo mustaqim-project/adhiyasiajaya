609f46e7e3ea6ae18478acbb779230aae2b0960f
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-14 12:05:51 +0700

    jhsjkdhfkjsdhfjkhwdekj

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,30 +2,34 @@
 <header>
     <div class="header-area">
         <!-- Header Top Area -->
-
-
-        <div class="header-top_area d-none d-lg-block"
-            style="background-color: #001133; padding: 10px 0; color: #fff; font-size: 14px;">
+        <div class="header-top_area d-none d-lg-block">
             <div class="container">
                 <div class="row align-items-center">
-                    <!-- Logo and Contact Info Section -->
                     <div class="col-xl-4 col-lg-4">
                         <div class="logo">
-                            <div class="short_contact_list">
-                                <i class="fa fa-phone mr-1"></i> <a href="tel:{{ @$contact->phone }}"
-                                    style="color: #fff; text-decoration: none;">{{ @$contact->phone }}</a>
-                                <i class="fa fa-envelope ml-4 mr-1"></i> <a href="mailto:{{ @$contact->email }}"
-                                    style="color: #fff; text-decoration: none;">{{ @$contact->email }}</a>
-                            </div>
+                            <a href="{{ url('/') }}">
+                                <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid">
+                            </a>
                         </div>
                     </div>
-
-                    <!-- Request Button Section -->
                     <div class="col-xl-8 col-lg-8">
                         <div class="header_right d-flex align-items-center">
+                            <div class="short_contact_list">
+                                <ul>
+                                    <li>
+                                        <a href="mailto:{{ @$contact->email }}">
+                                            <i class="fa fa-envelope"></i> {{ @$contact->email }}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="tel:{{ @$contact->phone }}">
+                                            <i class="fa fa-phone"></i> {{ @$contact->phone }}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                             <div class="book_btn d-none d-lg-block">
-                                <a href="{{ route('contact') }}" style="text-decoration: none; color: #ffffff;">GET A
-                                    QUOTE <i class="fa fa-paper-plane"></i></a>
+                                <a class="boxed-btn3-line" href="{{ route('contact') }}">Request Now</a>
                             </div>
                         </div>
                     </div>
@@ -33,15 +37,14 @@
             </div>
         </div>
 
-
         <!-- Sticky Header -->
         <div id="sticky-header" class="main-header-area">
             <div class="container">
                 <div class="header_bottom_border">
-                    <div class="row align-items-center justify-content-between">
+                    <div class="row align-items-center">
 
-                        <!-- Logo -->
-                        <div class="col-xl-3 col-lg-3 col-6">
+                        <!-- Mobile Logo -->
+                        <div class="col-12 d-block d-lg-none">
                             <div class="logo">
                                 <a href="{{ url('/') }}">
                                     <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid"
@@ -50,136 +53,73 @@
                             </div>
                         </div>
 
-                        <!-- Mobile Menu Toggle -->
-                        <div class="col-6 d-lg-none text-end">
-                            <button class="mobile-menu-toggle">
-                                <i class="ti-menu"></i>
-                            </button>
-                        </div>
-
                         <!-- Main Navigation -->
-                        <div class="col-xl-9 col-lg-9 d-none d-lg-block">
-                            <div class="main-menu">
+                        <div class="col-xl-9 col-lg-9">
+                            <div class="main-menu d-none d-lg-block">
                                 <nav>
                                     <ul id="navigation"
-                                        style="display: flex; justify-content: flex-end; margin: 0; padding: 0;">
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ url('/') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">HOME</a>
-                                        </li>
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ route('about') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">ABOUT US</a>
-                                        </li>
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ route('product') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">PRODUCTS</a>
-                                        </li>
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ route('catalog') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">CATALOG</a>
-                                        </li>
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ route('quote') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">GET A QUOTE</a>
-                                        </li>
-                                        <li style="margin: 0 15px;">
-                                            <a href="{{ route('contact') }}"
-                                                style="text-transform: uppercase; font-weight: bold;">CONTACT</a>
-                                        </li>
-                                    </ul>
+                                    style="display: flex; justify-content: flex-end; margin: 0; padding: 0;">
+                                    <li style="margin: 0 15px;">
+                                        <a href="{{ url('/') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">HOME</a>
+                                    </li>
+                                    <li style="margin: 0 15px;">
+                                        <a href="{{ route('about') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">ABOUT US</a>
+                                    </li>
+                                    <li style="margin: 0 15px;">
+                                        <a href="{{ route('product') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">PRODUCTS</a>
+                                    </li>
+                                    {{-- <li style="margin: 0 15px;">
+                                        <a href="{{ route('catalog') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">CATALOG</a>
+                                    </li>
+                                    <li style="margin: 0 15px;">
+                                        <a href="{{ route('quote') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">GET A QUOTE</a>
+                                    </li> --}}
+                                    <li style="margin: 0 15px;">
+                                        <a href="{{ route('contact') }}"
+                                            style="text-transform: uppercase; font-weight: bold;">CONTACT</a>
+                                    </li>
+                                    <li style="margin: 0 15px;">
+                                        <!-- Search Button -->
+                                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                                            <div class="Appointment justify-content-end">
+                                                <div class="search_btn">
+                                                    <a data-toggle="modal" data-target="#searchModal"
+                                                        href="#">
+                                                        <i class="ti-search"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
                                 </nav>
                             </div>
                         </div>
 
-                        <!-- Mobile Navigation -->
-                        <div class="mobile-menu d-lg-none" style="display: none;">
-                            <nav>
-                                <ul id="mobile-navigation" style="list-style: none; padding: 0; margin: 0;">
-                                    <li>
-                                        <a href="{{ url('/') }}">HOME</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('about') }}">ABOUT US</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('product') }}">PRODUCTS</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('catalog') }}">CATALOG</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('quote') }}">GET A QUOTE</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('contact') }}">CONTACT</a>
-                                    </li>
-                                </ul>
-                            </nav>
+                        <!-- Search Button -->
+                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                            <div class="Appointment justify-content-end">
+                                <div class="search_btn">
+                                    <a data-toggle="modal" data-target="#searchModal" href="#">
+                                        <i class="ti-search"></i>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
 
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- Mobile Menu Toggle Script -->
-        <script>
-            document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
-                const mobileMenu = document.querySelector('.mobile-menu');
-                mobileMenu.style.display = mobileMenu.style.display === 'block' ? 'none' : 'block';
-            });
-        </script>
-
-        <!-- Responsive CSS -->
-        <style>
-            #sticky-header {
-                position: sticky;
-                top: 0;
-                z-index: 1000;
-                background: #fff;
-                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-            }
-
-            .main-header-area {
-                padding: 10px 0;
-            }
-
-            .mobile-menu {
-                background-color: #fff;
-                padding: 10px;
-            }
-
-            .mobile-menu ul li {
-                margin: 10px 0;
-            }
-
-            .mobile-menu ul li a {
-                text-transform: uppercase;
-                font-weight: bold;
-                color: #333;
-                text-decoration: none;
-            }
-
-            .mobile-menu ul li a:hover {
-                color: #007bff;
-            }
-
-            .mobile-menu-toggle {
-                background: none;
-                border: none;
-                font-size: 24px;
-                cursor: pointer;
-                color: #333;
-            }
-
-            @media (max-width: 991px) {
-                .main-menu {
-                    display: none;
-                }
-            }
-        </style>
-
     </div>
 </header>
 <!-- Header End -->
