a66980ec8707fbac0481b99290d1f79e92a4a69c
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-14 14:58:07 +0700

    asdfasdf234234

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -47,6 +47,15 @@
     <div class="service_area">
         <div class="container">
             <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title mb-50 text-center">
+                        <h3>
+                            Our Product
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-md-6 col-lg-4">
                     <div class="single_service">
                         <div class="thumb">
@@ -148,13 +157,52 @@
     </div>
     {{-- End Our Product --}}
 
+
+
+    <!-- chose_area  -->
+    <div class="chose_area">
+        <div class="container">
+            <div class="features_main_wrap">
+                <div class="row  align-items-center">
+                    <div class="col-xl-5 col-lg-5 col-md-6">
+                        <div class="about_image">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoxX5Y_tFFAdfN84VkJ9iyB4Ux3nBdjLQmbQ&s"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <div class="features_info">
+                            <h3>About Us</h3>
+                            <p>PT. Adhya Asia Jaya was established in June 2023. The founders have been engaged in the Pulp
+                                & Paper Industry for 18 years. As an Indonesian private-owned company, we specialize in the
+                                supply of chemicals, machinery & parts, and laboratory testing instruments to the Indonesia
+                                Pulp & Paper Industry.</p>
+                            <p>We serve as an Agent, Distributor, and Stockist for companies from Austria, Hong Kong/USA,
+                                Singapore, Australia, Italy, Canada, and China, catering to the entire Indonesian market.
+                                With a presence in 3 cities, we ensure comprehensive coverage of the Indonesia Pulp & Paper
+                                market.</p>
+                            <ul>
+                                <li> Long-term partnerships with global and local partners. </li>
+                                <li> Committed to supporting customer needs with quality products and services. </li>
+                                <li> Creating value for customers and principals through innovation and reliability. </li>
+                            </ul>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ chose_area  -->
+
     <div class="service_area" style="background-color: #2596be">
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
                     <div class="section_title mb-50 text-center">
                         <h3>
-                            Services We Offer
+                            Our Customer
                         </h3>
                     </div>
                 </div>
@@ -203,45 +251,6 @@
             </div>
         </div>
     </div>
-
-    <!-- chose_area  -->
-    <div class="chose_area">
-        <div class="container">
-            <div class="features_main_wrap">
-                <div class="row  align-items-center">
-                    <div class="col-xl-5 col-lg-5 col-md-6">
-                        <div class="about_image">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoxX5Y_tFFAdfN84VkJ9iyB4Ux3nBdjLQmbQ&s"
-                                alt="">
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6">
-                        <div class="features_info">
-                            <h3>About Us</h3>
-                            <p>PT. Adhya Asia Jaya was established in June 2023. The founders have been engaged in the Pulp
-                                & Paper Industry for 18 years. As an Indonesian private-owned company, we specialize in the
-                                supply of chemicals, machinery & parts, and laboratory testing instruments to the Indonesia
-                                Pulp & Paper Industry.</p>
-                            <p>We serve as an Agent, Distributor, and Stockist for companies from Austria, Hong Kong/USA,
-                                Singapore, Australia, Italy, Canada, and China, catering to the entire Indonesian market.
-                                With a presence in 3 cities, we ensure comprehensive coverage of the Indonesia Pulp & Paper
-                                market.</p>
-                            <ul>
-                                <li> Long-term partnerships with global and local partners. </li>
-                                <li> Committed to supporting customer needs with quality products and services. </li>
-                                <li> Creating value for customers and principals through innovation and reliability. </li>
-                            </ul>
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ chose_area  -->
-
-
 
     <!-- Estimate_area start  -->
     <div class="Estimate_area overlay">
