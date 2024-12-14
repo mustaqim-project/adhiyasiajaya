76d1e321ec2c077a485db8f3f9302dbd0703b2db
mustaqim-project <agungmustaqim15@gmail.com>
2024-12-15 03:03:51 +0700

    asdfasdf234234

--- resources/views/admin/setting_landing_page/index.blade.php
+++ resources/views/admin/setting_landing_page/index.blade.php
@@ -10,83 +10,76 @@
             <div class="card-header">
                 <h4>{{ __('admin.Setting Landing Page') }}</h4>
             </div>
+            @php
+            $setting = App\Models\SettingLandingPage::first(); // Mengambil entri pertama
+        @endphp
 
             <div class="card-body">
 
-                <div class="tab-content tab-bordered" id="myTab3Content">
-                    @php
-                        $setting = App\Models\SettingLandingPage::first(); // Mengambil entri pertama
-                    @endphp
+                <form action="{{ route('admin.setting_landing_page.update', ['id' => $setting->id]) }}"
+                    method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT') <!-- Gunakan metode PUT untuk update -->
 
-                    <div class="tab-pane fade show" role="tabpanel" aria-labelledby="home-tab2">
-                        <div class="card-body">
-                            <form action="{{ route('admin.setting_landing_page.update', ['id' => $setting->id]) }}"
-                                method="POST" enctype="multipart/form-data">
-                                @csrf
-                                @method('PUT') <!-- Gunakan metode PUT untuk update -->
+                    <!-- ID (Hidden field) -->
+                    <input type="hidden" name="id" value="{{ $setting->id }}">
 
-                                <!-- ID (Hidden field) -->
-                                <input type="hidden" name="id" value="{{ $setting->id }}">
-
-                                <!-- Image Slide 1 -->
-                                <div class="form-group">
-                                    <label for="image_slide1">{{ __('admin.Image Slide 1') }}</label>
-                                    <input type="file" name="image_slide1" class="form-control">
-                                    <input type="text" name="link_slide1" class="form-control"
-                                        value="{{ $setting->link_slide1 ?? '' }}"
-                                        placeholder="{{ __('admin.Link Slide 1') }}">
-                                    <textarea name="desc_slide1" class="form-control" placeholder="{{ __('admin.Description Slide 1') }}" cols="30"
-                                        rows="3">{{ $setting->desc_slide1 ?? '' }}</textarea>
-                                </div>
-
-                                <!-- Image Slide 2 -->
-                                <div class="form-group">
-                                    <label for="image_slide2">{{ __('admin.Image Slide 2') }}</label>
-                                    <input type="file" name="image_slide2" class="form-control">
-                                    <input type="text" name="link_slide2" class="form-control"
-                                        value="{{ $setting->link_slide2 ?? '' }}"
-                                        placeholder="{{ __('admin.Link Slide 2') }}">
-                                    <textarea name="desc_slide2" class="form-control" placeholder="{{ __('admin.Description Slide 2') }}" cols="30"
-                                        rows="3">{{ $setting->desc_slide2 ?? '' }}</textarea>
-                                </div>
-
-                                <!-- Image About -->
-                                <div class="form-group">
-                                    <label for="image_about">{{ __('admin.Image About') }}</label>
-                                    <input type="file" name="image_about" class="form-control">
-                                </div>
-
-                                <!-- Background Contact -->
-                                <div class="form-group">
-                                    <label for="bg_contact">{{ __('admin.Background Contact') }}</label>
-                                    <input type="file" name="bg_contact" class="form-control">
-                                </div>
-
-                                <!-- Image Header About -->
-                                <div class="form-group">
-                                    <label for="image_header_about">{{ __('admin.Image Header About') }}</label>
-                                    <input type="file" name="image_header_about" class="form-control">
-                                </div>
-
-                                <!-- Image Header Product -->
-                                <div class="form-group">
-                                    <label for="image_header_product">{{ __('admin.Image Header Product') }}</label>
-                                    <input type="file" name="image_header_product" class="form-control">
-                                </div>
-
-                                <!-- Image Header Contact -->
-                                <div class="form-group">
-                                    <label for="image_header_contact">{{ __('admin.Image Header Contact') }}</label>
-                                    <input type="file" name="image_header_contact" class="form-control">
-                                </div>
-
-
-
-                                <button type="submit" class="btn btn-primary">{{ __('admin.Save') }}</button>
-                            </form>
-                        </div>
+                    <!-- Image Slide 1 -->
+                    <div class="form-group">
+                        <label for="image_slide1">{{ __('admin.Image Slide 1') }}</label>
+                        <input type="file" name="image_slide1" class="form-control">
+                        <input type="text" name="link_slide1" class="form-control"
+                            value="{{ $setting->link_slide1 ?? '' }}"
+                            placeholder="{{ __('admin.Link Slide 1') }}">
+                        <textarea name="desc_slide1" class="form-control" placeholder="{{ __('admin.Description Slide 1') }}" cols="30"
+                            rows="3">{{ $setting->desc_slide1 ?? '' }}</textarea>
                     </div>
-                </div>
+
+                    <!-- Image Slide 2 -->
+                    <div class="form-group">
+                        <label for="image_slide2">{{ __('admin.Image Slide 2') }}</label>
+                        <input type="file" name="image_slide2" class="form-control">
+                        <input type="text" name="link_slide2" class="form-control"
+                            value="{{ $setting->link_slide2 ?? '' }}"
+                            placeholder="{{ __('admin.Link Slide 2') }}">
+                        <textarea name="desc_slide2" class="form-control" placeholder="{{ __('admin.Description Slide 2') }}" cols="30"
+                            rows="3">{{ $setting->desc_slide2 ?? '' }}</textarea>
+                    </div>
+
+                    <!-- Image About -->
+                    <div class="form-group">
+                        <label for="image_about">{{ __('admin.Image About') }}</label>
+                        <input type="file" name="image_about" class="form-control">
+                    </div>
+
+                    <!-- Background Contact -->
+                    <div class="form-group">
+                        <label for="bg_contact">{{ __('admin.Background Contact') }}</label>
+                        <input type="file" name="bg_contact" class="form-control">
+                    </div>
+
+                    <!-- Image Header About -->
+                    <div class="form-group">
+                        <label for="image_header_about">{{ __('admin.Image Header About') }}</label>
+                        <input type="file" name="image_header_about" class="form-control">
+                    </div>
+
+                    <!-- Image Header Product -->
+                    <div class="form-group">
+                        <label for="image_header_product">{{ __('admin.Image Header Product') }}</label>
+                        <input type="file" name="image_header_product" class="form-control">
+                    </div>
+
+                    <!-- Image Header Contact -->
+                    <div class="form-group">
+                        <label for="image_header_contact">{{ __('admin.Image Header Contact') }}</label>
+                        <input type="file" name="image_header_contact" class="form-control">
+                    </div>
+
+
+
+                    <button type="submit" class="btn btn-primary">{{ __('admin.Save') }}</button>
+                </form>
             </div>
         </div>
     </section>
