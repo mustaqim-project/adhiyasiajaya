edf8e63f7b4b3fe7a4ecae50f8014541e088d7cc
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-14 13:22:20 +0700

    sdfgsdfgsdf

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,33 +2,35 @@
 <header>
     <div class="header-area">
         <!-- Header Top Area -->
-        <div class="header-top_area d-none d-lg-block"
-            style="background-color: #001133; padding: 10px 0; color: #fff; font-size: 14px;">
+        <div class="header-top_area d-none d-lg-block" style="background-color: #001133; padding: 10px 0; color: #fff; font-size: 14px;">
             <div class="container">
                 <div class="row align-items-center">
                     <!-- Logo and Contact Info Section -->
                     <div class="col-xl-4 col-lg-4">
-                        <div class="short_contact_list">
+                        <ul class="short_contact_list d-flex align-items-center" style="list-style: none; padding: 0; margin: 0;">
                             @foreach ($socialLinks as $link)
-                                <a href="{{ $link->url }}" class="social__link">
-                                    <i class="{{ $link->icon }}"></i>
-                                </a>
+                                <li style="margin-right: 10px;">
+                                    <a href="{{ $link->url }}" class="social__link" style="color: #fff;">
+                                        <i class="{{ $link->icon }}"></i>
+                                    </a>
+                                </li>
                             @endforeach
-                            <i class="fa fa-phone mr-1"></i>
-                            <a href="tel:{{ @$contact->phone }}"
-                                style="color: #fff; text-decoration: none;">{{ @$contact->phone }}</a>
-                            <i class="fa fa-envelope ml-4 mr-1"></i>
-                            <a href="mailto:{{ @$contact->email }}"
-                                style="color: #fff; text-decoration: none;">{{ @$contact->email }}</a>
-                        </div>
+                            <li style="margin-right: 10px;">
+                                <i class="fa fa-phone mr-1"></i>
+                                <a href="tel:{{ @$contact->phone }}" style="color: #fff; text-decoration: none;">{{ @$contact->phone }}</a>
+                            </li>
+                            <li>
+                                <i class="fa fa-envelope mr-1"></i>
+                                <a href="mailto:{{ @$contact->email }}" style="color: #fff; text-decoration: none;">{{ @$contact->email }}</a>
+                            </li>
+                        </ul>
                     </div>
 
                     <!-- Request Button Section -->
                     <div class="col-xl-8 col-lg-8">
                         <div class="header_right d-flex align-items-center justify-content-end">
                             <div class="book_btn">
-                                <a class="boxed-btn3-line" href="{{ route('contact') }}"
-                                    style="text-decoration: none; color: #ffffff;">
+                                <a class="boxed-btn3-line" href="{{ route('contact') }}" style="text-decoration: none; color: #ffffff;">
                                     GET A QUOTE <i class="fa fa-paper-plane"></i>
                                 </a>
                             </div>
@@ -37,6 +39,7 @@
                 </div>
             </div>
         </div>
+
 
         <!-- Sticky Header -->
         <div id="sticky-header" class="main-header-area">
@@ -48,8 +51,7 @@
                         <div class="col-12 d-block d-lg-none">
                             <div class="logo">
                                 <a href="{{ url('/') }}">
-                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid"
-                                        width="180px">
+                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid" width="180px">
                                 </a>
                             </div>
                         </div>
@@ -58,8 +60,7 @@
                         <div class="col-xl-3 col-lg-3 col-6 d-none d-lg-block">
                             <div class="logo">
                                 <a href="{{ url('/') }}">
-                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid"
-                                        width="400px">
+                                    <img src="{{ asset($settings['site_logo']) }}" alt="Logo" class="img-fluid" width="400px">
                                 </a>
                             </div>
                         </div>
@@ -68,8 +69,7 @@
                         <div class="col-xl-6 col-lg-6">
                             <div class="main-menu d-none d-lg-block">
                                 <nav>
-                                    <ul id="navigation" class="d-flex align-items-center"
-                                        style="text-transform: uppercase; font-weight: bold; justify-content: flex-end; margin: 0; padding: 0;">
+                                    <ul id="navigation" class="d-flex align-items-center" style="text-transform: uppercase; font-weight: bold; justify-content: flex-end; margin: 0; padding: 0;">
 
                                         <li><a href="{{ url('/') }}" class="menu-link">HOME</a></li>
                                         <li><a href="{{ route('about') }}" class="menu-link">ABOUT US</a></li>
