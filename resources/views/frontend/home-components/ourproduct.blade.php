28932e28d5960dd01a891e5fc3cac77344a6e172
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-15 22:12:34 +0700

    asdfasd

--- resources/views/frontend/home-components/ourproduct.blade.php
+++ resources/views/frontend/home-components/ourproduct.blade.php
@@ -11,8 +11,8 @@
             </div>
         </div>
         <div class="row">
-            {{-- Ambil hanya 5 kategori --}}
-            @foreach ($categories as $category)
+            {{-- Fetch only 5 categories --}}
+            @foreach ($categories->take(5) as $category)
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="single_service">
                         <div class="thumb">
@@ -24,23 +24,25 @@
                         </div>
                         <div class="service_info">
                             <h3><a href="#">{{ $category->name }}</a></h3>
-                            <p>{{ $category->deskripsi }}</p>
+                            <p>{{ $category->deskripsi ?? 'No description available.' }}</p>
                         </div>
                     </div>
                 </div>
             @endforeach
 
+            {{-- No categories fallback --}}
             @if ($categories->isEmpty())
                 <div class="col-12">
-                    <p class="text-center">No categories found.</p>
+                    <p class="text-center mt-4 mb-4">No categories found.</p>
                 </div>
             @endif
 
-
-                        <a href="{{ route('product') }}" class="btn-our-product">
-                            Our Product
-                        </a>
-
+            {{-- Our Product Button --}}
+            <div class="col-12 text-center mt-4">
+                <a href="{{ route('product') }}" class="btn btn-primary btn-our-product">
+                    Our Product
+                </a>
+            </div>
         </div>
     </div>
 </div>
