51e9ba6690b4442d8db52e938e5d3f48b2cfb67f
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-15 22:35:19 +0700

    dfgsd2345345

--- resources/views/frontend/news-details.blade.php
+++ resources/views/frontend/news-details.blade.php
@@ -10,46 +10,57 @@
 @section('meta_tw_description', $news->meta_description)
 @section('meta_tw_image', asset($news->image))
 <!-- End Setting metas -->
+
 @section('content')
-    <section class="pb-80">
+
+    <!-- bradcam_area -->
+    <div class="bradcam_area bradcam_bg_2">
         <div class="container">
             <div class="row">
-                <!-- bradcam_area -->
-                <div class="bradcam_area bradcam_bg_2">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xl-12">
-                                <div class="bradcam_text text-center">
-                                    <h3>Our Product</h3>
-                                </div>
-                            </div>
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <h3>Our Product</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ bradcam_area -->
+
+    <!-- service_details_start -->
+    <div class="service_details_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <div class="service_details_left">
+                        <h3>Market Sector</h3>
+                        <div class="nav nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            @foreach ($categories as $category)
+                                <a class="nav-link {{ $loop->first ? 'active' : '' }}"
+                                    id="v-pills-{{ $category->slug }}-tab" data-toggle="pill"
+                                    href="#v-pills-{{ $category->slug }}" role="tab"
+                                    aria-controls="v-pills-{{ $category->slug }}"
+                                    aria-selected="{{ $loop->first ? 'true' : 'false' }}">
+                                    {{ $category->name }}
+                                </a>
+                            @endforeach
                         </div>
                     </div>
                 </div>
-                <!--/ bradcam_area -->
-
                 <div class="col-md-8">
-                    <!-- content article detail -->
-                    <!-- Article Detail -->
                     <div class="wrap__article-detail">
                         <div class="wrap__article-detail-title">
-                            <h1>
-                                {!! $news->title !!}
-                            </h1>
-
+                            <h1>{!! $news->title !!}</h1>
                         </div>
                         <hr>
                         <div class="wrap__article-detail-info">
                             <ul class="list-inline d-flex flex-wrap justify-content-start">
                                 <li class="list-inline-item">
                                     {{ __('frontend.By') }}
-                                    <a href="#">
-                                        {{ $news->auther->name }}
-                                    </a>
+                                    <a href="#">{{ $news->auther->name }}</a>
                                 </li>
                                 <li class="list-inline-item">
                                     <span class="text-dark text-capitalize ml-1">
-
                                         {{ date('M D, Y', strtotime($news->created_at)) }}
                                     </span>
                                 </li>
@@ -57,11 +68,7 @@
                                     <span class="text-dark text-capitalize">
                                         {{ __('frontend.in') }}
                                     </span>
-                                    <a href="#">
-                                        {{ $news->category->name }}
-                                    </a>
-
-
+                                    <a href="#">{{ $news->category->name }}</a>
                                 </li>
                             </ul>
                         </div>
@@ -75,9 +82,7 @@
                             <div class="total-views">
                                 <div class="total-views-read">
                                     {{ convertToKFormat($news->views) }}
-                                    <span>
-                                        {{ __('frontend.views') }}
-                                    </span>
+                                    <span>{{ __('frontend.views') }}</span>
                                 </div>
 
                                 <ul class="list-inline">
@@ -89,7 +94,6 @@
                                             <i class="fa fa-facebook-f"></i>
                                             <span>{{ __('frontend.facebook') }}</span>
                                         </a>
-
                                     </li>
                                     <li class="list-inline-item">
                                         <a class="btn btn-social-o twitter"
@@ -115,7 +119,6 @@
                                             <span>{{ __('frontend.telegram') }}</span>
                                         </a>
                                     </li>
-
                                     <li class="list-inline-item">
                                         <a class="btn btn-linkedin-o linkedin"
                                             href="https://www.linkedin.com/shareArticle?mini=true&url={{ url()->current() }}&title={{ $news->title }}"
@@ -124,76 +127,16 @@
                                             <span>{{ __('frontend.linkedin') }}</span>
                                         </a>
                                     </li>
-
                                 </ul>
                             </div>
-                            <p class="has-drop-cap-fluid">
-                                {!! $news->content !!}
-                            </p>
+                            <p class="has-drop-cap-fluid">{!! $news->content !!}</p>
                         </div>
-
-
                     </div>
                     <!-- end content article detail -->
-
-
-
-
-
                 </div>
             </div>
-    </section>
+        </div>
+    </div>
+    <!-- service_details_end -->
+
 @endsection
-
-@push('content')
-    <script>
-        $(document).ready(function() {
-            $('.delete-msg').on('click', function(e) {
-                e.preventDefault();
-                let id = $(this).data('id');
-                Swal.fire({
-                    title: '{{ __('frontend.Are you sure?') }}',
-                    text: "{{ __("frontend.You won'\t be able to revert this!") }}",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: '{{ __('frontend.Yes, delete it!') }}'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-
-                        $.ajax({
-                            method: 'DELETE',
-                            url: "{{ route('product-comment-destroy') }}",
-                            data: {
-                                id: id
-                            },
-                            success: function(data) {
-                                if (data.status === 'success') {
-                                    Swal.fire(
-                                        'Deleted!',
-                                        data.message,
-                                        'success'
-                                    )
-                                    window.location.reload();
-                                } else if (data.status === 'error') {
-                                    Swal.fire(
-                                        'Error!',
-                                        data.message,
-                                        'error'
-                                    )
-                                }
-                            },
-                            error: function(xhr, status, error) {
-                                console.error(error);
-                            }
-                        });
-
-
-                    }
-                })
-            })
-
-        })
-    </script>
-@endpush
