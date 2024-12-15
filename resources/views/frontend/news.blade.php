fddf0b7ea97e4e7901a32a4eaf23e6b0d8a7d86b
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-15 22:24:06 +0700

    asdfasd234

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -51,9 +51,9 @@
                                                         alt="{{ $news->title }}" class="img-fluid" />
                                                 </div>
                                                 <div class="service_info">
-                                                    <h3><a
+                                                    <h5><a
                                                             href="{{ route('product-details', $news->slug) }}">{{ $news->title }}</a>
-                                                    </h3>
+                                                    </h5>
                                                     <p>{!! Str::limit($news->content, 200, '...') !!}</p>
                                                 </div>
                                             </div>
