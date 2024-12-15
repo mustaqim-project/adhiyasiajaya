cfadb15e98b110578893dfe197ecfa8af6e35572
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-15 22:06:27 +0700

    asdf234234

--- resources/views/frontend/home-components/ourproduct.blade.php
+++ resources/views/frontend/home-components/ourproduct.blade.php
@@ -1,14 +1,11 @@
 {{-- Our Product --}}
-
 <div class="service_area">
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
                 <div class="section_title mb-50 text-center">
                     <a href="{{ route('product') }}">
-                    <h3>
-                        Market Sector
-                    </h3>
+                        <h3>Market Sector</h3>
                     </a>
                 </div>
             </div>
@@ -19,8 +16,11 @@
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="single_service">
                         <div class="thumb">
-                            <img width="200px" src="{{ $category->image ? asset($category->image) : asset('default-image.jpg') }}"
-                                alt="{{ $category->name }}" />
+                            <img
+                                src="{{ $category->image ? asset($category->image) : asset('default-image.jpg') }}"
+                                alt="{{ $category->name }}"
+                                class="img-fluid"
+                                style="max-width: 100%; height: auto; object-fit: cover;" />
                         </div>
                         <div class="service_info">
                             <h3><a href="#">{{ $category->name }}</a></h3>
@@ -30,24 +30,21 @@
                 </div>
             @endforeach
 
-
             @if ($categories->isEmpty())
                 <div class="col-12">
                     <p class="text-center">No categories found.</p>
                 </div>
             @endif
 
-            {{-- <div class="col-md-6 col-lg-4">
+            <div class="col-md-6 col-lg-4">
                 <div class="single_service">
                     <div class="service_info">
-                        <button class="btn-our-product">
+                        <a href="{{ route('product') }}" class="btn-our-product">
                             Our Product
-                        </button>
+                        </a>
                     </div>
                 </div>
-            </div> --}}
-
-
+            </div>
         </div>
     </div>
 </div>
