a4200f76c598acc1633e60f497843258231ffed4
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-15 18:36:22 +0700

    sdfgsdf345345

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -43,41 +43,6 @@
 </div>
 <!--/ chose_area  -->
 
-<!-- counter_area  -->
-<div class="counter_area">
-    <div class="container">
-        <div class="offcan_bg">
-            <div class="row">
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_counter text-center">
-                        <h3> <span class="counter">42</span> <span>+</span> </h3>
-                        <p>Countries Covered</p>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_counter text-center">
-                        <h3> <span class="counter">97</span> <span>+</span> </h3>
-                        <p>Business Success</p>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_counter text-center">
-                        <h3> <span class="counter">2342</span></h3>
-                        <p>Happy Client</p>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_counter text-center">
-                        <h3> <span class="counter">3245</span></h3>
-                        <p>Business Done</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
-
 
 
 
