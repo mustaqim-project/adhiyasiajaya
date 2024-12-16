20f1a979e9f874ae733a32a6b8c691b84538b29c
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-16 17:27:19 +0700

    asdfasdfasdf234

--- resources/views/frontend/home-components/brand.blade.php
+++ resources/views/frontend/home-components/brand.blade.php
@@ -1,5 +1,5 @@
 {{-- Our Product --}}
-<div class="service_area" style="padding: 40px 0 40px 0; background-color: #cedbf3;" >
+<div class="service_area" style="padding: 40px 0 40px 0; background-color: #cedbf3 important!;" >
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
