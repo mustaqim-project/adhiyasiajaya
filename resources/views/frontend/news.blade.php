6a3d9dec3ddebaf8a8dfeffd82d858be63e968fb
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-16 10:46:09 +0700

    asdfasd34234

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -14,7 +14,83 @@
         </div>
     </div>
     <!--/ bradcam_area -->
+    <div class="blog_page_search">
+        <form action="{{ route('news') }}" method="GET">
+            <div class="row">
+                <div class="col-lg-5">
+                    <input type="text" placeholder="Type here" value="{{ request()->search }}" name="search">
+                </div>
+                <div class="col-lg-4">
+                    <select name="category">
+                        <option value="">{{ __('frontend.All') }}</option>
+                        @foreach ($categories as $category)
+                            <option {{ $category->slug === request()->category ? 'selected' : '' }}
+                                value="{{ $category->slug }}">{{ $category->name }}</option>
+                        @endforeach
 
+                    </select>
+                </div>
+                <div class="col-lg-3">
+                    <button type="submit">{{ __('frontend.search') }}</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <aside class="wrapper__list__article ">
+        @if (request()->has('category'))
+            <h4 class="border_section">{{ __('frontend.Category') }}: {{ request()->category }}</h4>
+        @endif
+
+        <div class="row">
+            @foreach ($news as $post)
+                <div class="col-lg-6">
+                    <!-- Post Article -->
+                    <div class="article__entry">
+                        <div class="article__image">
+                            <a href="{{ route('news-details', $post->slug) }}">
+                                <img src="{{ asset($post->image) }}" alt="" class="img-fluid">
+                            </a>
+                        </div>
+                        <div class="article__content">
+                            <div class="article__category">
+                                {{ $post->category->name }}
+                            </div>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <span class="text-primary">
+                                        {{ __('frontend.by') }} {{ $post->auther->name }}
+                                    </span>
+                                </li>
+                                <li class="list-inline-item">
+                                    <span class="text-dark text-capitalize">
+                                        {{ date('M d, Y', strtotime($post->created_at)) }}
+                                    </span>
+                                </li>
+
+                            </ul>
+                            <h5>
+                                <a href="{{ route('news-details', $post->slug) }}">
+                                    {!! truncate($post->title) !!}
+                                </a>
+                            </h5>
+                            <p>
+                                {!! truncate($post->content, 100) !!}
+                            </p>
+                            <a href="{{ route('news-details', $post->slug) }}"
+                                class="btn btn-outline-primary mb-4 text-capitalize"> {{ __('frontend.read more') }}</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            @if (count($news) === 0)
+                <div class="text-center w-100">
+                    <h4>{{ __('frontend.No News Found') }} :(</h4>
+                </div>
+            @endif
+        </div>
+
+    </aside>
     <!-- service_details_start -->
     <div class="service_details_area">
         <div class="container">
