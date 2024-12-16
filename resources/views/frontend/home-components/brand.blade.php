0f8d0e02d0b8b90a75c67edaa3cc0019baf56033
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-16 15:53:57 +0700

    asdfasdf234234

--- resources/views/frontend/home-components/brand.blade.php
+++ resources/views/frontend/home-components/brand.blade.php
@@ -4,14 +4,13 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="section_title mb-50 text-left">
-                    <a href="{{ route('product') }}">
+                    <a href="{{ route('brand') }}">
                         <h3>Brands</h3>
                     </a>
                 </div>
             </div>
         </div>
         <div class="row">
-            {{-- Loop hanya untuk 5 kategori --}}
             @foreach ($brands->take(30) as $brand)
             <div class="col-md-6 col-lg-4 mb-4">
                 <div class="single_service">
@@ -26,17 +25,11 @@
                 </div>
             </div>
             @endforeach
-
-
             @if ($categories->isEmpty())
                 <div class="col-12">
                     <p class="text-center">No categories found.</p>
                 </div>
             @endif
-
-
-
-
         </div>
         <a href="{{ route('brand') }}" class="boxed-btn3" width="100%" style="display: block;">
             View More </a>
