935d8377f7f70c94fbf21d88a86cfab620f16a9b
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-16 10:43:22 +0700

    asdfasd

--- resources/views/frontend/news-details.blade.php
+++ resources/views/frontend/news-details.blade.php
@@ -321,7 +321,7 @@
                 <div class="col-lg-4">
                     <div class="blog_right_sidebar">
                         <aside class="single_sidebar_widget search_widget">
-                            <form action="#">
+                            <form action="{{ route('product') }}" method="GET">
                                 <div class="form-group">
                                     <div class="input-group mb-3">
                                         <input type="text" class="form-control" placeholder='Search Keyword'
