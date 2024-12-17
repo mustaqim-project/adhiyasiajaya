3a506755cb73699e87d811b2e236a359e8b1688e
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-12-17 16:50:53 +0700

    asdfasd234234

--- app/Http/Controllers/Admin/KatalogController.php
+++ app/Http/Controllers/Admin/KatalogController.php
@@ -6,6 +6,7 @@ use App\Models\Katalog;
 use App\Models\Category; // Pastikan Anda mengimpor model Category
 use Illuminate\Http\Request;
 use App\Traits\FileUploadTrait;
+use App\Http\Controllers\Controller;
 
 class KatalogController extends Controller
 {
