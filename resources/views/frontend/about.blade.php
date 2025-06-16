0924f40ba016671ab8ba1070663370d386daf19d
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-06-16 11:34:23 +0700

    asdf234asdf

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -3,6 +3,46 @@
 @section('content')
 <section>
 
+    <!-- bradcam_area  -->
+    <div class="bradcam_area bradcam_bg_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <h3>About Us</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ bradcam_area  -->
+
+<!-- chose_area  -->
+<div class="chose_area ">
+    <div class="container">
+        <div class="features_main_wrap">
+            <div class="row  align-items-center">
+                <div class="col-xl-5 col-lg-5 col-md-6">
+                    <div class="about_image">
+                        <img src="{{ asset($settingpage->image_about) }}" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6">
+                    <div class="features_info">
+                        <h3>About Us?</h3>
+                        {!! @$about->content !!}
+
+                        <div class="about_btn">
+                            <a class="boxed-btn3-line" href="{{ route('contact') }}">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/ chose_area  -->
+
 
     <!-- About Us Section -->
     <section id="about" class="section-padding bg-neutral">
